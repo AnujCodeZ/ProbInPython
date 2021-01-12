6ce11c0c4ddb6a49007226b6775464075b8053a3
AnujCodeZ <anujrana.sid55@gmail.com>
2021-01-12 20:26:41 +0530

    correlated random variables

--- 03-continuous_random_variables.py
+++ 03-continuous_random_variables.py
@@ -12,3 +12,23 @@ plt.show()
 # In scipy
 X = stats.uniform(-2, 4)
 print(X.pdf(1.2))
+
+# Correlated random variables
+var1 = 1
+var2 = 1
+cov = 0.9
+cov_matrix = [[var1, cov],
+              [cov, var2]]
+
+data = np.random.multivariate_normal([0, 0], cov_matrix, size=1000)
+
+plt.scatter(data[:, 0], data[:, 1])
+plt.show()
+
+N = 1000
+x = np.random.normal(size=N)
+eps = np.random.normal(size=N)
+y = x + 0.5 * eps
+
+plt.scatter(x, y)
+plt.show()
